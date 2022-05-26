feaf745c072a0f67e507d9b74dc13938082360dc
Tetiana <bta1985@ukr.net>
2022-05-26 19:45:38 +0300

    add prettier

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from "react-dom/client";
 
 const Header = () => <h1>Hello App.js</h1>;
 
-function AppList () {
+function AppList() {
     return (
-            <ul>
-                <li>list item 1</li>
-                <li>list item 2</li>
-                <li>list item 3</li>
-            </ul>
-       );
+        <ul>
+            <li>list item 1</li>
+            <li>list item 2</li>
+            <li>list item 3</li>
+        </ul>
+    );
 }
 
 function App() {
-   return (
-       <>
-        <Header/>
-        <AppList/>
-       </>
-   ) ;
- }
+    return (
+        <>
+            <Header />
+            <AppList />
+        </>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
