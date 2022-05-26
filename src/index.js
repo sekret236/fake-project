691a6fa634fa3fbdc11de321de48f065ad7bd6e1
Tetiana <bta1985@ukr.net>
2022-05-26 19:07:04 +0300

    create way

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello App.js</h1>;
-}
+    return (
+        <>
+            <h1>Hello App.js</h1>
+            <ul>
+                <li>list item 1</li>
+                <li>list item 2</li>
+                <li>list item 3</li>
+            </ul>
+        </>
+    );
+ }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
