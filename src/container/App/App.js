1f146c3a7c208565264462fb61692f8fe50a22e9
Tetiana <bta1985@ukr.net>
2022-06-01 20:59:17 +0300

    add mmaterial-mui

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,12 +1,16 @@
-import { Footer } from 'container/Footer/Footer';
-import { Header } from 'container/Header/Header';
-import { Main } from 'container/Main/Main';
-import React from 'react';
+import React from 'react'
+
+import { Footer } from 'container/Footer/Footer'
+import { Header } from 'container/Header/Header'
+import { Main } from 'container/Main/Main'
+import { CssBaseline } from '@mui/material'
+
 
 
 export const App = () => {
     return (
         <>
+            <CssBaseline/>        
             <Header/>
             <Main/>
             <Footer/>
