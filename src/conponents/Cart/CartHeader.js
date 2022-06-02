aa514fe30da6a54516c37805a0d4c2adaaa33b0c
Tetiana <bta1985@ukr.net>
2022-06-02 19:55:23 +0300

    add sass

--- src/conponents/Cart/CartHeader.js
+++ src/conponents/Cart/CartHeader.js
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from './CartHeader.module.css'
+import React from 'react'
+import styles from './CartHeader.scss'
 
 export const CartHeader = () => {
     return (
-        <div className = {styles.CartHeader}>
+        <div className="cartHeader">
             <div>0</div>
             <div>0$</div>
         </div>
